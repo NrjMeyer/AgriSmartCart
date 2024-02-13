655d0e268036b585294e8a0ca81f7248dc3c2345
ToavinaL <hlahatrafampiononana@gmail.com>
2024-02-13 09:25:06 +0300

    User auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,12 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'firstname',
+        'address',
         'email',
         'password',
+        'photo',
+        'role'
     ];
 
     /**
